30852cde976c405e3badb38546a175a3e23374f7
Валентина Стражникова <vvalentina_@hotmail.com>
2023-02-19 20:57:24 +0200

    Add Task_8

---
+++ DevBy/HomePage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+namespace DevBy
+{
+	public class HomePage
+	{
+		IWebDriver _driverDevBy;
+
+		const string SITE_PARTS_JAVA_VACANCIES = "//a[@title='Java']";
+		const string SITE_PARTS_JAVA_VACANCIES_COUNT = "//a[@title='Java']/following-sibling::div";
+
+		public HomePage(IWebDriver driverDevBy)
+		{
+			_driverDevBy = driverDevBy;
+			_driverDevBy.Url = "https://devby.io/";
+		}
+
+		internal void OpenJavaVacancies()
+		{
+			_driverDevBy.FindElement(By.XPath(SITE_PARTS_JAVA_VACANCIES)).Click();
+		}
+
+		internal int GetCountJavaVacanciesHomePage()
+		{
+			var javaVacanсiesAtHomePage = _driverDevBy.FindElement(By.XPath(SITE_PARTS_JAVA_VACANCIES_COUNT));
+
+			try
+			{ 
+				int countJavaVacanсiesHomePage = Int32.Parse(javaVacanсiesAtHomePage.Text.Trim().Split(' ')[0]);
+				return countJavaVacanсiesHomePage;
+			}
+			catch (FormatException e)
+			{
+				Console.WriteLine(e.Message);
+			}
+
+			return 0;
+		}
+		
+	}
+}
---
+++ DevBy/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using DevBy;
+
+WebDriver driverDevBy = new ChromeDriver();
+
+driverDevBy.Manage().Window.Maximize();
+
+HomePage homePage = new HomePage(driverDevBy);
+
+int countJavaVacanciesHomePage = homePage.GetCountJavaVacanciesHomePage();
+
+SpecializationJava pageJava = new SpecializationJava(driverDevBy);
+
+var countVacanciesJavaPage = pageJava.GetCountJavaVacancies();
+
+Console.WriteLine(countVacanciesJavaPage.Equals(countJavaVacanciesHomePage));
+
+driverDevBy.Close();
